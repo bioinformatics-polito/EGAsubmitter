232a567862ee22f9c96423fcde3dc015475861b8
mviviani <marco.viviani@ircc.it>
2022-03-08 11:59:53 +0100

    some modifications done for the crypting and upload part

--- local/share/snakerule/Snakefile_upload.smk
+++ local/share/snakerule/Snakefile_upload.smk
@@ -1,10 +1,10 @@
-include: "../snakemake/conf_upload.sk"
+# include: "../snakemake/conf_upload.sk"
 
-rule fastq_upload:
-    input: lambda wildcards: METADATA[wildcards.batch]
-    output: touch("fastq_upload-{batch}.done")
-    params: tool=PDX2GODOT, batch="{batch}", outpath=OUTPUT_DIR
-    shell:
-        """
-            {params.tool} {params.batch} {input} {params.outpath}
-        """
+# rule fastq_upload:
+#     input: lambda wildcards: METADATA[wildcards.batch]
+#     output: touch("fastq_upload-{batch}.done")
+#     params: tool=PDX2GODOT, batch="{batch}", outpath=OUTPUT_DIR
+#     shell:
+#         """
+#             {params.tool} {params.batch} {input} {params.outpath}
+#         """
--- local/src/Runs_build.R
+++ local/src/Runs_build.R
@@ -24,7 +24,7 @@ yaml <- read_yaml(text = yamlTemplate)
 yaml$runFileTypeId <- snakemake@params[['runType']] ### TODO given by the user
 
 mainDir <- snakemake@params[['path']]
-EGACryptor <- "../local/share/data/EGACryptor"
+EGACryptor <- paste0(mainDir,"/crypting-uploading/EGACryptor")
 metadataDir <- paste0(mainDir,"/user_folder/metadata")
 samplesDir <- paste0(metadataDir,"/samples")
 runsDir <- paste0(metadataDir,"/runs")
@@ -63,11 +63,11 @@ for ( s in seq(csv[,"alias"]) ) {
   yaml[["files"]][[1]][["checksum"]] <- readLines(gpg[basename(gpg)==checksum], n=1, warn=FALSE)
   yaml[["files"]][[1]][["unencryptedChecksum"]] <- readLines(md5[basename(md5)==unencryptedChecksum], n=1, warn=FALSE)
   check <- list(c(checksum, unencryptedChecksum)) #fileName
-  # for ( i in seq(check) ) {
-  #   if ( !check[i] %in% basename(list.files(path=EGACryptor, recursive=TRUE, full.names=FALSE)) ) {
-  #   stop(paste("Sorry, a file from the crypting phase is missing for the sample",sample))
-  #   }
-  # }
+  for ( i in seq(check) ) {
+    if ( !check[i] %in% basename(list.files(path=EGACryptor, recursive=TRUE, full.names=FALSE)) ) {
+    stop(paste("Sorry, a file from the crypting phase is missing for the sample",sample))
+    }
+  }
   json <- toJSON(yaml, auto_unbox=TRUE, na="string", pretty=TRUE)
   write(json, paste0(runsDir,"/Run_",sample,".json"))
   txt <- append(txt, paste0(logsDir,"/done/runs/",sample,"-runSubmission.done"))
