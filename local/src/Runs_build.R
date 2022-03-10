57628f4853d850467ea0a0b31d93643c0161e595
mviviani <marco.viviani@ircc.it>
2022-03-10 15:21:35 +0100

    bla

--- local/src/Runs_build.R
+++ local/src/Runs_build.R
@@ -35,7 +35,7 @@ logsDir <- paste0(mainDir,"/submission/logs")
 ### .csv passed by the user with all samples' informations
 # csvNaive = list.files(path=metadataDir, pattern="*.tsv", full.names=TRUE)
 csv <- read.csv(paste0(metadataDir,"/Samples_Informations.tsv"), stringsAsFactors = FALSE)
-rightOrder <- c("alias","title","description","caseOrControlId","genderId","organismPart","cellLine","region","phenotype","subjectId","anonymizedName","bioSampleId","sampleAge","sampleDetail","attributes.tag","attributes.value","fileName","filePaths")
+rightOrder <- c("alias","title","description","caseOrControlId","genderId","organismPart","cellLine","region","phenotype","subjectId","anonymizedName","bioSampleId","sampleAge","sampleDetail","attributes.tag","attributes.value","fileName","filePath")
 if ( !all(colnames(csv) ==  rightOrder) )  {
   stop("The columns of the file you provide must be in the exact same order we gave in the template.\nPlease, order them accordingly.")
 }
