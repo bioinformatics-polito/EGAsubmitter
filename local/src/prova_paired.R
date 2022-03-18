7494a1a67e04fc43d9181f62b5e09c0d2582a789
mviviani <marco.viviani@ircc.it>
2022-03-18 17:48:24 +0100

    new buildRuns for paired

--- local/src/prova_paired.R
+++ local/src/prova_paired.R
@@ -18,7 +18,7 @@ logsDir <- paste0(mainDir,"/submission/logs")
 
 
 wantPaired <- FALSE
-exp <- read_yaml(paste0(metadataDir,"/Experiment.yaml"))
+exp <- read_yaml(paste0(metadataDir,"/yaml/Experiment.yaml"))
 if ( exp[["libraryLayoutId"]]==1 ) {
   wantPaired <- TRUE
 }
@@ -62,7 +62,7 @@ yaml <- read_yaml(text=yamlTemplate)
 yaml$runFileTypeId <- snakemake@params[['runType']] ### TODO given by the user
 
 ### .csv passed by the user with all samples' informations
-csv <- read.csv(paste0(metadataDir,"/Samples_Informations.csv"), stringsAsFactors = FALSE)
+csv <- read.csv(paste0(metadataDir,"/Samples_Informations.csv"), header=TRUE, quote='""',  stringsAsFactors = FALSE)
 rightOrder <- c("alias","title","description","caseOrControlId","genderId","organismPart","cellLine","region","phenotype","subjectId","anonymizedName","bioSampleId","sampleAge","sampleDetail","attributes.tag","attributes.value","fileName","filePath")
 if ( !all(colnames(csv) ==  rightOrder) )  {
   stop("The columns of the file you provide must be in the exact same order we gave in the template.\nPlease, order them accordingly.")
@@ -70,7 +70,7 @@ if ( !all(colnames(csv) ==  rightOrder) )  {
 
 ### lists for all checksums' files
 gpg <- list.files(path=EGACryptor, pattern="*.gpg.md5", recursive=TRUE, full.names=TRUE)
-md5 <- list.files(path=EGACryptor, recursive=TRUE, pattern="*.gz.md5", full.names=TRUE)
+md5 <- list.files(path=EGACryptor, pattern="*.gz.md5", recursive=TRUE, full.names=TRUE)
 
 ### I take these two informations for the later submission
 sink(paste0(metadataDir,"/title"))
@@ -79,14 +79,14 @@ sink()
 sink(paste0(metadataDir,"/description"))
 writeLines(csv$description[1])
 sink()
-
-txt <- NULL
+### check this loop, but it kinda works
 if ( wantPaired ) {
-    r1 <- csv[grep("R1", csv$fileName),]
-    r2 <- csv[grep("R2", csv$fileName),]
-    for ( s in seq(r1[,"alias"]) ) {
+    for ( s in seq(csv[,"alias"]) ) {
         sample <- csv[s,"alias"]
-        file <- csv[s,"fileName"]
+        tmp <- csv[csv$alias==sample,]
+        r1 <- tmp[grep("R1", tmp$fileName),]
+        r2 <- tmp[grep("R2", tmp$fileName),]
+        file <- r1[,"fileName"]
         checksum <- paste0(file,".gpg.md5")
         unencryptedChecksum <- paste0(file,".md5")
         yaml[["files"]][[1]][["fileId"]] <- file
@@ -97,10 +97,7 @@ if ( wantPaired ) {
         if ( !checksum %in% basename(gpg) | !unencryptedChecksum %in% basename(md5) ) {
             stop(paste("Sorry, a file from the crypting phase is missing for the sample",sample))
         }
-    }
-    for ( s in seq(r2[,"alias"]) ) {
-        sample <- csv[s,"alias"]
-        file <- csv[s,"fileName"]
+        file <- r2[,"fileName"]
         checksum <- paste0(file,".gpg.md5")
         unencryptedChecksum <- paste0(file,".md5")
         yaml[["files"]][[2]][["fileId"]] <- file
@@ -111,10 +108,9 @@ if ( wantPaired ) {
         if ( !checksum %in% basename(gpg) | !unencryptedChecksum %in% basename(md5) ) {
             stop(paste("Sorry, a file from the crypting phase is missing for the sample",sample))
         }
+        json <- toJSON(yaml, auto_unbox=TRUE, na="string", pretty=TRUE)
+        write(json, paste0(runsDir,"/Run_",sample,".json"))
     }
-    json <- toJSON(yaml, auto_unbox=TRUE, na="string", pretty=TRUE)
-    write(json, paste0(runsDir,"/Run_",sample,".json"))
-    txt <- append(txt, paste0(logsDir,"/done/runs/",sample,"-runSubmission.done"))
 } else {
     for ( s in seq(csv[,"alias"]) ) {
         sample <- csv[s,"alias"]
@@ -131,11 +127,9 @@ if ( wantPaired ) {
         }
         json <- toJSON(yaml, auto_unbox=TRUE, na="string", pretty=TRUE)
         write(json, paste0(runsDir,"/Run_",sample,".json"))
-        txt <- append(txt, paste0(logsDir,"/done/runs/",sample,"-runSubmission.done"))
-}
+    }
 }
 
-write.table(txt, paste0(runsDir,"/Allfiles_list.txt"), quote=FALSE, row.names=FALSE, col.names=FALSE)
 
 csv$filePath <- NULL # we remove this column for the json
 csv$fileName <- NULL # we remove this column for the json
@@ -144,17 +138,20 @@ csv <- csv[!duplicated(csv$alias),]
 write.csv(csv, file=paste0(samplesDir,"/SamplesInformations.csv"), row.names=FALSE)
 
 ### produces files lists for submissionfunctions
+getRun <- NULL
 getJson <- NULL
 getSample <- NULL
 # getExps <- NULL
 # allsamples <- unique(csv$alias)
 for ( r in 1:nrow(csv) ) {
   sample <- paste0(csv[r,"alias"])
+  getRun <- append(getRun, paste0(logsDir,"/done/runs/",sample,"-runSubmission.done"))
   getJson <- append(getJson, paste0(samplesDir,"/",sample,".json"))
   getSample <- append(getSample, paste0(logsDir,"/done/samples/",sample,"-sampleSubmission.done"))
   # getExps <- append(getExps, paste0(logsDir,"/done/exps/",sample,"-experimentSubmission.done"))
 }
 
+write.table(getRun, paste0(runsDir,"/Allfiles_list.txt"), quote=FALSE, row.names=FALSE, col.names=FALSE)
 write.table(getJson, paste0(metadataDir,"/AllSamples_list.txt"), quote=FALSE, row.names=FALSE, col.names=FALSE)
 write.table(getSample, paste0(samplesDir,"/Allfiles_list.txt"), quote=FALSE, row.names=FALSE, col.names=FALSE)
 # write.table(getExps, paste0(expsDir,"/AllExps_list.txt"), quote=FALSE, row.names=FALSE, col.names=FALSE)
