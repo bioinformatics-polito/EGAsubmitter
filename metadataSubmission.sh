bdd90867148df793d509ae9cfe9001729404d9df
mviviani <marco.viviani@ircc.it>
2022-03-21 12:39:00 +0100

    added the prompt to ask what file type is being submitted

--- metadataSubmission.sh
+++ metadataSubmission.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+
+printf "Please, select the type (number) of your files:\n
+0 for BAM
+1 for Complete Genomics
+2 for CRAM
+3 for Fasta
+4 for One Fastq file (Single)
+5 for Two Fastq files (Paired)
+6 for PacBio HDF5
+7 for SFF
+8 for SRF
+
+File type value: "
+
+read fileType
+
+export FILETYPE=$fileType
+
 snakemake -s local/share/snakerule/Snakefile_submission
