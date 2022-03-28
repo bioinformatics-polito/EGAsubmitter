53d21ef5de506dfc876c53a633374330df8771a2
mviviani <marco.viviani@ircc.it>
2022-03-28 15:19:51 +0200

    new .sh to delete files and added ancient() to submission snakefile inputs to avoid nasty pipeline restarts

---
+++ delete_EncryptedFiles.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+while true; do
+    read -p "Are you sure you want to delete all the encrypted files?
+WARNING: If you did not finish the transfer and metadata uploading, you will need to re-encrypt them " yn
+    case $yn in
+        [Yy]* ) find dataset/encrypting-uploading/ ! -name *.gitkeep -type f -delete; echo "All files have been deleted"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please, answer y|yes or n|no.";;
+    esac
+done
---
+++ delete_SubmissionMetadata.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+while true; do
+    read -p "Are you sure you want to delete all files in submission folder?
+WARNING: This will restart the whole pipeline: if you still need to upload something do not delete them yet" yn
+    case $yn in
+        [Yy]* ) find dataset/submission/ ! -name *.gitkeep -type f -delete; echo "All files have been deleted"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please, answer y|yes or n|no.";;
+    esac
+done
+
+
+while true; do
+    read -p "Are you sure you want to delete all files in user_folder folder?
+NOTE: Your .csv and .yaml files will not be deleted anyway " yn
+    case $yn in
+        [Yy]* ) find dataset/user_folder/ ! -name *.yaml ! -name *.gitkeep ! -name *.csv -type f -delete; echo "All files have been deleted"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please, answer y|yes or n|no.";;
+    esac
+done
