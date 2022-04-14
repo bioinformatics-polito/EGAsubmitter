176147447045b136df5ae0544b817539c8d69668
Elena Grassi <grassi.e@gmail.com>
2022-04-14 18:20:11 +0200

    First version of Dockerfile, working up to encrypt_upload
    Right now testing on occam/Singularity.

--- getPaths.sh
+++ getPaths.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat dataset/user_folder/metadata/Samples_Informations.csv | tr "," "\t" | cut -f1,17,18 > dataset/user_folder/metadata/Samples_Informations_3cols.tsv
+cat dataset/user_folder/metadata/Samples_Informations.csv | sed 1d |  tr "," "\t" | cut -f1,17,18 > dataset/user_folder/metadata/Samples_Informations_3cols.tsv
