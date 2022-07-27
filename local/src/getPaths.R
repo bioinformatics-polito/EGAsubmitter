aa6729a76b83ebf92924a0ea1f1875d8838720fb
mviviani <marco.viviani@ircc.it>
2022-07-27 17:41:20 +0200

    trying

---
+++ local/src/getPaths.R
@@ -0,0 +1,23 @@
+### #!/usr/bin/env Rscript
+
+df_in <- snakemake@input[[1]]
+paths_out <- snakemake@output[['paths']]
+names_out <- snakemake@output[['samples']]
+
+df <- read.table(df_in, sep=",",header = TRUE)
+
+fn <- df$fileName
+
+all <- data.frame()
+names(all) <- "all_names"
+for ( name in fn ) {
+  all[nrow(all)+1,1] <- paste0("/scratch/trcanmed/EGAv2/EGAsubmitter/dataset/encrypting-uploading/EGACryptor",name,".gpg")
+  all[nrow(all)+1,1] <- paste0("/scratch/trcanmed/EGAv2/EGAsubmitter/dataset/encrypting-uploading/EGACryptor",name,".gpg.md5")
+  all[nrow(all)+1,1] <- paste0("/scratch/trcanmed/EGAv2/EGAsubmitter/dataset/encrypting-uploading/EGACryptor",name,".md5")
+}
+
+write.table(all, names_out,sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+
+
+paths <- as.data.frame(df$filePath)
+write.table(paths, paths_out, sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
