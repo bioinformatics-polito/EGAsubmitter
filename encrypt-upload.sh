95b3671ffa2e5fcbe7c0d2cda824485e1fd31139
mviviani <marco.viviani@ircc.it>
2022-07-28 15:54:44 +0200

    fixing

--- encrypt-upload.sh
+++ encrypt-upload.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-printf "Name of the project (folder): "
-read project
+# printf "Name of the project (folder): "
+# read project
 
 printf "How many cores do you want to use?: "
 read cores
 
 printf "\n"
 
-export PROJECT_NAME=$project
+# export PROJECT_NAME=$project
 snakemake -s local/share/snakerule/Snakefile_encrypting-uploading --cores $cores
