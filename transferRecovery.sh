cacd37746519949ee976799b57c6b3384de4575f
mviviani <marco.viviani@ircc.it>
2022-10-06 11:40:11 +0200

    fixed transfer recovery rule

--- transferRecovery.sh
+++ transferRecovery.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+printf "Name of the project (folder): "
+read project
+
+printf "How many cores do you want to use?: "
+read cores
+
+printf "\n"
+
+export PROJECT_NAME=$project
 snakemake -s local/share/snakerule/Snakefile_transferRecovery
