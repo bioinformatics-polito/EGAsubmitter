73f81098b2a5a0864205e0902802d3b5e7d16afc
mviviani <marco.viviani@ircc.it>
2022-10-18 11:06:58 +0200

    possible problem avoided when uploading fastq and bam together: now getPaths starts from _3cols instead

--- local/src/getPaths.R
+++ local/src/getPaths.R
@@ -4,16 +4,16 @@ df_in <- snakemake@input[[1]]
 names_out <- snakemake@output[['samples']]
 path <- paste0(snakemake@params[[1]],"/")
 
-df <- read.table(df_in, sep=",",header = TRUE)
+df <- read.table(df_in, sep='\t', quote="", header=FALSE)
 
 # fn <- df$alias
 
 all <- data.frame()
 
 for ( n in 1:nrow(df) ) {
-  all[nrow(all)+1,1] <- paste0(path,df[n,"alias"],"/",df[n,"fileName"],".gpg")
-  all[nrow(all)+1,1] <- paste0(path,df[n,"alias"],"/",df[n,"fileName"],".gpg.md5")
-  all[nrow(all)+1,1] <- paste0(path,df[n,"alias"],"/",df[n,"fileName"],".md5")
+  all[nrow(all)+1,1] <- paste0(path,df[n,1],"/",df[n,2],".gpg")
+  all[nrow(all)+1,1] <- paste0(path,df[n,1],"/",df[n,2],".gpg.md5")
+  all[nrow(all)+1,1] <- paste0(path,df[n,1],"/",df[n,2],".md5")
 }
 
 write.table(all, names_out, sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
