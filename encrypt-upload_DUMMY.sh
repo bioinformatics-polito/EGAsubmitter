cc10cfc544aa30a47efa04eb9e6275bb136eea86
Elena Grassi <data@cog.pupidi>
2023-02-20 10:39:36 +0100

    Dry run script for metadata submission

--- encrypt-upload_DUMMY.sh
+++ encrypt-upload_DUMMY.sh
@@ -6,6 +6,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+echo "${RED}WARNING${NC}: you are using a dummy/dry run script that will only encrypt files to test EGAsubmitter,  \n use encrypt-upload.sh to work on real datasets."
+
 printf "Name of the project (folder): "
 read project
 
@@ -18,15 +20,14 @@ export PROJECT_NAME=$project
 rm -f dataset/encrypting-uploading/logs/done/filesCrypted.done
 rm -f dataset/encrypting-uploading/logs/done/paths.done
 rm -f dataset/user_folder/metadata/All_files-names.txt
-echo "${RED}WARNING${NC}: you are using a dummy/dry run script that will only encrypt files to test EGAsubmitter,  \n use encrypt-upload.sh to work on real datasets."
 snakemake -s local/share/snakerule/Snakefile_encrypting-uploading --cores $cores all_crypted
 
-echo "${RED}Checking if all the files have been crypted...${NC}"
+echo "Checking if all the files have been crypted..."
 n=$(sort dataset/user_folder/metadata/All_files-names.txt | uniq | wc -l)
-# We check wheter the list of files contains 15 paths, since we have 5 example input files (1 encrypted files and two gpg each).
+# We check whether the list of files contains 15 paths, since we have 5 example input files (1 encrypted files and two gpg each).
 if [ "$n" -ne "15" ]
 then
-  echo "Something failed in the test, we did not generate a complete list of encrypted files!${NC}"
+  echo "${RED}Something failed in the test, we did not generate a complete list of encrypted files!${NC}"
   echo "Encryption test: ${RED}failed${NC}"
 else
   echo "Encryption test: ${GREEN}passed${NC}"
