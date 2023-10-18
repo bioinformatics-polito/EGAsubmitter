5ec92143099800dc194baa9b17315e5b4b3fae98
mviviani <marco.viviani@ircc.it>
2023-10-18 18:42:22 +0200

    new modifications for the new EGA system

--- local/src/buildRuns.py
+++ local/src/buildRuns.py
@@ -22,12 +22,14 @@
 
 import argparse as ap
 import pandas as pd
-import json, glob, sys, os
+import json, glob, sys, os, re
+import random
 
 parser = ap.ArgumentParser(description="Build all the Runs object for the samples to upload")
 parser.add_argument("-p", "--path", help="Main path to store everything")
+parser.add_argument("-i", "--input", help="Dataframe with the Files provisional IDs")
 parser.add_argument("-j", "--template", help="Path to json templates for paired and single runs")
-parser.add_argument("-t", "--type", help="File type given by the user", type=int)
+parser.add_argument("-t", "--type", help="File type given by the user")
 parser.add_argument("-o", "--output", help="Output .csv file name")
 parser.add_argument("-d", "--done", help=".done file")
 
@@ -48,32 +50,31 @@ template = json.load(j)
 
 template['run_file_type'] = args.type
 
+df = pd.read_csv(os.path.join(metadataDir,"Files_IDs.tsv"), sep='\t', header=0, usecols=['provisional_id','relative_path']) ### Load the df with the files provisional IDs
+df['relative_path'] = df['relative_path'].str.lstrip('/')
+df['relative_path'] = df['relative_path'].str.rstrip('.c4gh')
+
 csv = pd.read_csv(os.path.join(metadataDir,"Samples_Information.csv"), sep=',', header=0) ### Load the .csv filled by the user
 ### The header must follow this order, or EGA will return an error when EGAsubmitter will upload the samples .json file
 csv.sort_values(by=['fileName'], axis=0, ascending=True, inplace=True)
-rightOrder = ["alias","title","description","caseOrControlId","genderId","organismPart","cellLine","region","phenotype","subjectId","anonymizedName","bioSampleId","sampleAge","sampleDetail","attributes.tag","attributes.value","fileName","filePath","fileName.bam","filePath.bam"]
+rightOrder = ["alias","title","description","biological_sex","subject_id","phenotype","biosample_id","case_control","organism_part","cell_line","fileName","filePath","fileName.bam","filePath.bam"] #,"extra_attributes.tag","extra_attributes.value","extra_attributes.unit"
 if ( any(csv.axes[1] != rightOrder) ):
     print("The columns of the file you provide must be in the exact same order we gave in the template.\nPlease, order them accordingly.")
     sys.exit()
 
-### lists for all checksums' files created during the encryption phase: this will be used later to recover the specific checksum value
-gpg = sorted(glob.glob(os.path.join(EGACryptor,"**/*.gpg.md5"), recursive=True))
-md5 = [f for f in sorted(glob.glob(os.path.join(EGACryptor,"**/*.md5"), recursive=True)) 
-         if not os.path.basename(f).endswith('.gpg.md5')]
+csv = csv.merge(df, left_on='fileName', right_on='relative_path', how='left')
 
 samples = set(csv['alias'])
 
-
-### Here I think I will need to iterate the files ID file and put the id # in "files"
-### but I still am not able to retrieve these IDs. For now leave this to see if the submission works
 for sample in csv['alias']:
-    if len(csv.loc[csv['alias']==sample]) != 1:
-        template['files'] = 2
+    tmp = csv.loc[csv['alias']==sample]
+    template['files'] = tmp['provisional_id'].tolist()
+    with open(os.path.join(runsDir,"Run_"+sample+".json"), 'w') as final:
+        json.dump(template, final, indent=2)
         
 ### These 4 columns were manually added to be used while specific portion of the tool, but for the creation of the Sample.json they must be removed, because are not recognized by EGA server
 csv = csv.drop(columns=['filePath', 'fileName', 'filePath.bam', 'fileName.bam'])
 csv.drop_duplicates(subset=['alias'], keep='first', inplace=True) ### In case there are paired fastq, here I remove one row, cause there should be only one Sample.json per sample.
-### if single-end is requested, this should not give problems anyway
 
 csv.to_csv(samplesDir+"/SamplesInformation.csv", header=True, index=False)
 
