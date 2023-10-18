b519d6494f7e0b636b1b66ccd9dd0c62f6bf3c19
mviviani <marco.viviani@ircc.it>
2023-10-18 18:59:26 +0200

    change columns in getPaths.sh

--- getPaths.sh
+++ getPaths.sh
@@ -6,10 +6,10 @@ Answer "yes" if you encrypted and uploaded .fastq files already and you want to 
 Answer "no" to encrypt and upload other files for the first time    
 ' yn
     case $yn in
-        [Yy]* ) cat dataset/user_folder/metadata/Samples_Information.csv | tr "," "\t" | sed 1d | cut -f1,16,17 > dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp;
+        [Yy]* ) cat dataset/user_folder/metadata/Samples_Information.csv | tr "," "\t" | sed 1d | cut -f1,13,14 > dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp;
                 sed 's/\"//g' dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp > dataset/user_folder/metadata/Samples_Information_3cols.tsv;
                 rm dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp; break;;
-        [Nn]* ) cat dataset/user_folder/metadata/Samples_Information.csv | tr "," "\t" | sed 1d | cut -f1,14,15 > dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp;
+        [Nn]* ) cat dataset/user_folder/metadata/Samples_Information.csv | tr "," "\t" | sed 1d | cut -f1,11,12 > dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp;
                 sed 's/\"//g' dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp > dataset/user_folder/metadata/Samples_Information_3cols.tsv;
                 rm dataset/user_folder/metadata/Samples_Information_3cols.tsv.tmp; break;;
         * ) echo "Please, answer y|yes or n|no.";;
