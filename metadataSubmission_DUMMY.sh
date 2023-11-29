6b56d76175038701dedc0c77ed3e891948825fc6
mviviani <marco.viviani@ircc.it>
2023-11-29 17:22:04 +0100

    modified dummy

--- metadataSubmission_DUMMY.sh
+++ metadataSubmission_DUMMY.sh
@@ -13,15 +13,17 @@ printf "Name of the project (folder): "
 read project
 
 printf "Please, select the type (number) of your files:\n
-0 for BAM
-1 for Complete Genomics
-2 for CRAM
-3 for Fasta
-4 for One Fastq file (Single)
-5 for Two Fastq files (Paired)
-6 for PacBio HDF5
-7 for SFF
-8 for SRF
+- srf
+- sff
+- fastq
+- Illumina_native
+- Illumina_native_qseq
+- SOLiD_native_csfasta
+- PacBio_HDF5
+- bam
+- cram
+- CompleteGenomics_native
+- OxfordNanopore_native
 
 File type value: "
 
