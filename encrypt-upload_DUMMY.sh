21cb5dda457d11c2cb23d84629c83d72a1bce736
mviviani <marco.viviani@ircc.it>
2023-11-29 18:01:19 +0100

    fixes

--- encrypt-upload_DUMMY.sh
+++ encrypt-upload_DUMMY.sh
@@ -20,12 +20,12 @@ export PROJECT_NAME=$project
 rm -f dataset/encrypting-uploading/logs/done/filesCrypted.done
 rm -f dataset/encrypting-uploading/logs/done/paths.done
 rm -f dataset/user_folder/metadata/All_files-names.txt
-snakemake -s local/share/snakerule/Snakefile_encrypting-uploading --cores $cores all_crypted
+snakemake -s local/share/snakerule/Snakefile_SFTP_encrypting-uploading --cores $cores all_crypted
 
 echo "Checking if all the files have been crypted..."
 n=$(sort dataset/user_folder/metadata/All_files-names.txt | uniq | wc -l)
 # We check whether the list of files contains 15 paths, since we have 5 example input files (1 encrypted files and two gpg each).
-if [ "$n" -ne "15" ]
+if [ "$n" -ne "5" ]
 then
   echo "${RED}Something failed in the test, we did not list all encrypted files!${NC}"
   echo "Encryption list test: ${RED}failed${NC}"
@@ -33,8 +33,8 @@ else
   echo "Encryption list test: ${GREEN}passed${NC}"
 fi
 
-n=$(find dataset/encrypting-uploading/EGACryptor/${PROJECT_NAME}/ -type f \( -name '*.md5' -o -name '*.gpg' \) | wc -l)
-if [ "$n" -ne "15" ]
+n=$(find dataset/encrypting-uploading/crypt4gh/${PROJECT_NAME}/ -type f \( -name '*.c4gh' \) | wc -l)
+if [ "$n" -ne "5" ]
 then
   echo "${RED}Something failed in the test, we did not generate a complete list of encrypted files!${NC}"
   echo "Encryption test: ${RED}failed${NC}"
